8db2646f21b1756b159010f5bf1092dffa8dba67
Peter Parker <rajpootrajatrk@gmail.com>
2026-07-28 10:06:55 +0530

    changes

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("hello welcome FSD")
+// console.log("hello welcome FSD")
+
+// function sum(a,b) {
+//     return a+b;
+// }
+
+// function sum(a,b) {
+//     a = Math.sqrt(a);
+//     b = Math.sqrt(b);
+//     return (a,b);
+// }
+
+// const sum=function(a,b) {
+//     return a+b;
+// }
+
+// const sum=(a,b)=>{return a+b}
+
+// IIFE ( immediately inovke function experession)
+
+// (()=>{
+//     console.log("HEYY")
+// })();
+
+// console.log(sum(100,25));
