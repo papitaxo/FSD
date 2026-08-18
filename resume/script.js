53bcb7c75fd5971ffc2d98bdfa36c8dc0170a673
Peter Parker <rajpootrajatrk@gmail.com>
2026-08-18 10:29:38 +0530

    ON CLICK RESUME

--- resume/script.js
+++ resume/script.js
@@ -1,7 +1,7 @@
 const container=document.getElementById('container');
 const button=document.getElementById('btn');
 const heading= document.createElement('heading');
-heading.innerHTML="";
+heading.innerHTML="MY RESUME";
 
 function ping(){
     container.appendChild(heading);
