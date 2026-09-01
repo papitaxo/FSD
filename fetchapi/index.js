99f9b33aa045b525b34669fa3b8d431149a5a6d6
Peter Parker <rajpootrajatrk@gmail.com>
2026-09-01 09:59:05 +0530

    fetch api

--- fetchapi/index.js
+++ fetchapi/index.js
@@ -1,15 +1,19 @@
 // console.log("helo");
 
 const root = document.getElementById('container');
-const button = document.getElementById('btn')
-
+const button = document.getElementById('btn');
+const disp =document.getElementById('disp');
 
 async function fetchData(){
     // alert('hiii')
     
     try{
         const serverData = await fetch('https://fakestoreapi.com/products');
-        console.log(serverData);
+        // console.log(serverData);fetchapi/index.js
+            const jsonData = await serverData.json();
+            // console.log(jsonData);
+            // console.log(jsonData[0].title)
+            disp.innerHTML=`${jsonData[0].title}`;
     }catch(e){
         console.log("Error is "+e);
     }
